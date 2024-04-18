34afa06d402bdeeb6a33497974bc0f56946eba70
Aditya Dhanraj <opsingh861@gmail.com>
2024-04-18 20:26:11 +0530

    qr route

--- server/server.js
+++ server/server.js
@@ -3,44 +3,38 @@ const connectDB = require('./db/connection');
 const authRoutes = require('./routes/auth.route');
 const linkRoutes = require('./routes/link.route');
 const verifyToken = require('./utils/verifyUser');
-const { redirectToOriginalLink, incrementClicks } = require('./controllers/link.controller');
+const qrRoute = require('./routes/qr.route');
+const { redirectToOriginalLink } = require('./controllers/link.controller');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
+// API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/links', verifyToken, linkRoutes);
+app.use('/api/qr', qrRoute);
 
+// Default route
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/:shortUrl', redirectToOriginalLink);
-
-app.use('/api/auth', authRoutes);
-app.use('/api/links', verifyToken, linkRoutes);
-
-
-
-
-
+// Route for short URLs
 
 app.listen(3000, () => {
     connectDB();
     console.log('Server is running on port 3000');
 });
 
+app.get('/:shortUrl', redirectToOriginalLink);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        message,
-        statusCode,
-    });
+    return res.status(statusCode).json({ success: false, message, statusCode });
 });
