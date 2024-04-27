a20b02a23164575d384a5a1e19090021082ee357
Aditya Dhanraj <opsingh861@gmail.com>
2024-04-28 02:22:33 +0530

    download functionality added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'https://app.blinklink.fun', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 // API routes
 app.use('/api/auth', authRoutes);
