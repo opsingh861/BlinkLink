b362685dae8c02f896a9d5c1c8bc93eb79f2752b
Aditya Dhanraj <opsingh861@gmail.com>
2024-04-29 22:16:24 +0530

    updated axios instance

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://blinklink.fun/api/',
   withCredentials: true,
 });
 
